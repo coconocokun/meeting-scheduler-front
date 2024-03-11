8373019daee7f33bf9730b3691e96f4e1970f3af
GavinKwon <cocoforuiuc@gmail.com>
2024-03-11 16:11:47 +0900

    floating nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "How to use",
+      link: "/",
+    },
+    {
+      name: "Credits",
+      link: "/",
+    },
+  ];
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>LOGO</nav>
+        <FloatingNav navItems={navItems} />
         {children}
       </body>
     </html>
