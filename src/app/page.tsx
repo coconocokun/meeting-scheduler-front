469c191648c3b53440fa0999507289e358a69209
akisazaki <akisazaki@gmail.com>
2024-03-14 17:50:13 +0900

    section test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,8 +91,8 @@ const content = [
 export default function Home() {
   return (
     <>
-      <header className="h-[40rem] relative w-full from-black to-indigo-950 bg-gradient-to-b flex flex-col items-center justify-center overflow-hidden">
-        <div className="w-full absolute inset-0 h-screen">
+      <header className="relative w-full from-black to-indigo-950 bg-gradient-to-b flex flex-col items-center justify-center overflow-hidden h-screen">
+        <div className="w-full absolute inset-0 h-full">
           <SparklesCore
             id="tsparticlesfullpage"
             background="transparent"
@@ -115,7 +115,41 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <div id="learnMore"></div>
+      <div id="learnMore">
+        <section className="container mx-auto lg:max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 lg:gap-10 py-5 md:py-8 lg:py-10 items-stretch">
+          <div className="flex flex-col justify-center">
+            <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
+              Collaborative Editing
+            </div>
+          </div>
+          <div>
+            <h2 className="text-lg md:text-xl lg:text-3xl mb-3 md:mb-5 lg:mb-8 font-bold">Create a meeting schedule</h2>
+            <p>Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.</p>
+          </div>
+        </section>
+        <section className="container mx-auto lg:max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 lg:gap-10 py-5 md:py-8 lg:py-10 items-stretch">
+          <div className="flex flex-col justify-center">
+            <h2 className="text-lg md:text-xl lg:text-3xl mb-3 md:mb-5 lg:mb-8 font-bold">Invite guests</h2>
+            <p>See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.</p>
+          </div>
+          <Image
+            src={Screenshot1}
+            width={300}
+            height={300}
+            className="w-full object-cover aspect-[4/3]"
+            alt="linear board demo"
+          />
+        </section>
+        <section className="container mx-auto lg:max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 lg:gap-10 py-5 md:py-8 lg:py-10 items-stretch">
+          <div className="h-full w-full bg-gradient-to-br from-orange-500 to-yellow-500 flex items-center justify-center text-white">
+            Version control
+          </div>
+          <div className="flex flex-col justify-center">
+            <h2 className="text-lg md:text-xl lg:text-3xl mb-3 md:mb-5 lg:mb-8 font-bold">Set the final meeting time</h2>
+            <p>Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.</p>
+          </div>
+        </section>
+      </div>
       <StickyScroll content={content} />
       <MaskContainer
         revealText={
