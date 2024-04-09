9d8167a7bfa470c9a907bde2393b8580bf5984b3
Gavin Kwon <cocoforuiuc@gmail.com>
2024-04-09 15:29:25 +0900

    fixed description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { MaskContainer } from "@/components/ui/svg-mask-effect";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import "@/styles/home.css";
 
-
 export default function Home() {
   return (
     <>
@@ -14,7 +13,7 @@ export default function Home() {
           <div></div>
           <div className="relative">
             <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
-              Make awesome schedule
+              Find best time to meet together
             </h1>
             <div className="bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-full bottom-0 absolute blur-lg" />
             <div className="bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[1px] w-3/4 mt-1 mx-auto" />
@@ -34,9 +33,18 @@ export default function Home() {
             />
           </div>
           <div className="grid grid-cols-2 gap-16 text-center max-w-xl mx-auto relative z-20">
-            <a className="p-2 block bg-white border-indigo-600 border rounded font-bold" href="#learnMore">Learn more</a>
-            <a className="p-2 block text-white border-indigo-600 border bg-indigo-600 rounded font-bold"
-              href="/create">New meeting</a>
+            <a
+              className="py-2 px-4 block text-xl bg-white border-indigo-600 border rounded font-bold"
+              href="#learnMore"
+            >
+              Learn more
+            </a>
+            <a
+              className="py-2 px-4 block text-xl text-white border-indigo-600 border bg-indigo-600 rounded font-bold"
+              href="/create"
+            >
+              New meeting
+            </a>
           </div>
         </div>
       </AuroraBackground>
@@ -45,11 +53,14 @@ export default function Home() {
         revealText={
           <div className="space-y-10">
             <p className="max-w-4xl mx-auto text-slate-800 text-center text-3xl font-bold leading-normal">
-              This application is a collaborative creation by students from the Ziqzi Academy’s Computer Science introductory course.
-              Drawing inspiration from When2meet, the students have meticulously crafted the API and database architecture.</p>
+              This application is a collaborative creation by students from the Ziqzi Academy’s Computer Science
+              introductory course. Drawing inspiration from When2meet, the students have meticulously crafted the API
+              and database architecture.
+            </p>
             <p className="max-w-4xl mx-auto text-slate-800 text-center text-3xl font-bold leading-normal">
-              The backend is powered by a Node.js environment utilizing an Express server, while the frontend is developed with Next.js.
-              Despite having only three months of web development training, the students have commendably undertaken the majority of the backend development tasks.
+              The backend is powered by a Node.js environment utilizing an Express server, while the frontend is
+              developed with Next.js. Despite having only three months of web development training, the students have
+              commendably undertaken the majority of the backend development tasks.
             </p>
           </div>
         }
@@ -67,15 +78,15 @@ export default function Home() {
           <div className="px-32 space-y-20 border-l">
             <dl>
               <dt className="text-xl font-normal">Student</dt>
-              <dd>Json Kim</dd>
+              <dd>Jane Han</dd>
             </dl>
             <dl>
               <dt className="text-xl font-normal">Student</dt>
-              <dd>Mark Makaroni</dd>
+              <dd>Hyunwoo Kong</dd>
             </dl>
             <dl>
               <dt className="text-xl font-normal">Student</dt>
-              <dd>Jodern Ring</dd>
+              <dd>Joonyoung Jeon</dd>
             </dl>
           </div>
         </div>
@@ -84,17 +95,20 @@ export default function Home() {
         <WavyBackground className="max-w-4xl mx-auto pb-40">
           <div className="flex flex-col items-center">
             <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center mt-32">
-              Hero waves are cool
+              Start setting up a meeting
             </p>
             <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-              Leverage the power of canvas to create a beautiful hero section
+              Find the best meeting time or lecture time together
             </p>
-            <a className="block bg-indigo-600 text-white hover:bg-indigo-500 px-3 py-2 rounded mt-16"
-              href="/create">Create a new meeting schedule</a>
+            <a
+              className="block text-xl bg-indigo-600 text-white hover:bg-indigo-500 px-4 py-2 rounded mt-16"
+              href="/create"
+            >
+              Create a new meeting
+            </a>
           </div>
         </WavyBackground>
       </section>
-
     </>
   );
 }
--- src/components/float-image-section.tsx
+++ src/components/float-image-section.tsx
@@ -8,7 +8,6 @@ import Screenshot2 from "./../../public/images/screenshot2.png";
 import Screenshot3 from "./../../public/images/screenshot3.png";
 
 export default function FloatingImageSection() {
-
   const articlesRef = useRef<HTMLDivElement>(null);
   const [currentArticle, setCurrentArticle] = useState(0);
 
@@ -36,20 +35,27 @@ export default function FloatingImageSection() {
     return () => window.removeEventListener("scroll", onScroll);
   }, [articlesRef, setCurrentArticle]);
 
-
   return (
-    <div className={cn(
-      "transition-colors",
-      currentArticle !== 0 && "text-white",
-      currentArticle === 1 && "bg-indigo-900",
-      currentArticle === 2 && "bg-rose-900"
-    )}>
+    <div
+      className={cn(
+        "transition-colors",
+        currentArticle !== 0 && "text-white",
+        currentArticle === 1 && "bg-indigo-900",
+        currentArticle === 2 && "bg-rose-900"
+      )}
+    >
       <section id="learnMore" className="container mx-auto grid grid-cols-1 grid-rows-1">
         <div ref={articlesRef} className="col-start-1 row-start-1">
           <article className="grid grid-cols-[2fr_3fr] container mx-auto min-h-screen snap-always snap-center">
             <div className="flex flex-col justify-center">
-              <h2 className="text-lg md:text-5xl lg:text-7xl mb-3 md:mb-5 lg:mb-8 font-bold">Create a meeting schedule</h2>
-              <p className="md:text-lg lg:text-xl !leading-loose">Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.</p>
+              <h2 className="text-lg md:text-5xl lg:text-7xl mb-3 md:mb-5 lg:mb-8 font-bold">
+                Create a meeting schedule
+              </h2>
+              <p className="md:text-lg lg:text-xl !leading-loose">
+                If you are a host of the meeting, first set up a meeting by submitting the required form. You also
+                submit your preferred time, which will be displayed to guests so that they find the best time for them
+                too.
+              </p>
             </div>
             <div></div>
           </article>
@@ -57,20 +63,34 @@ export default function FloatingImageSection() {
             <div></div>
             <div className="flex flex-col justify-center">
               <h2 className="text-lg md:text-5xl lg:text-7xl mb-3 md:mb-5 lg:mb-8 font-bold">Invite guests</h2>
-              <p className="md:text-lg lg:text-xl !leading-loose">See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.</p>
+              <p className="md:text-lg lg:text-xl !leading-loose">
+                Simply copy the link of your created meeting, then share the link with your friends/guests. They will
+                enter the link to check whether their time is available or not.
+              </p>
             </div>
           </article>
           <article className="grid grid-cols-[2fr_3fr] container mx-auto min-h-screen snap-always snap-center">
             <div className="flex flex-col justify-center">
-              <h2 className="text-lg md:text-5xl lg:text-7xl mb-3 md:mb-5 lg:mb-8 font-bold">Set the final meeting time</h2>
-              <p className="md:text-lg lg:text-xl !leading-loose">Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.</p>
+              <h2 className="text-lg md:text-5xl lg:text-7xl mb-3 md:mb-5 lg:mb-8 font-bold">
+                Set the final meeting time
+              </h2>
+              <p className="md:text-lg lg:text-xl !leading-loose">
+                After guests submit their preferred time, find the best meeting time for the group. Let others know the
+                finalized meeting time.
+              </p>
             </div>
             <div></div>
           </article>
         </div>
         <div className="col-start-1 row-start-1" style={{ perspective: "100vw" }}>
-          <div className="sticky top-0 grid grid-cols-[2fr_3fr] transition-transform duration-500 ease-[cubic-bezier(.59,-0.34,0,1.34)]"
-            style={{ transform: `scaleY(${currentArticle === 1 ? "-1" : "1"}) rotateY(${currentArticle === 1 ? "180" : "-0"}deg)` }}>
+          <div
+            className="sticky top-0 grid grid-cols-[2fr_3fr] transition-transform duration-500 ease-[cubic-bezier(.59,-0.34,0,1.34)]"
+            style={{
+              transform: `scaleY(${currentArticle === 1 ? "-1" : "1"}) rotateY(${
+                currentArticle === 1 ? "180" : "-0"
+              }deg)`,
+            }}
+          >
             <div></div>
             <div className="grid grid-cols-1 grid-rows-1 min-h-screen items-center p-10">
               <Image
@@ -108,5 +128,5 @@ export default function FloatingImageSection() {
         </div>
       </section>
     </div>
-  )
+  );
 }
